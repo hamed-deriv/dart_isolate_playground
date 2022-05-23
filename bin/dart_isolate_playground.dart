161e00dd7669d10e7f90454674ec4cd50cc96198
hamed-deriv <57184669+hamed-deriv@users.noreply.github.com>
2022-05-23 16:19:04 +0800

    add some code

--- bin/dart_isolate_playground.dart
+++ bin/dart_isolate_playground.dart
@@ -1,1 +1,9 @@
-void main(List<String> arguments) {}
+import 'package:dart_isolate_playground/dart_isolate_playground.dart';
+
+void main(List<String> arguments) {
+  initializeIsolate(IsolateDate(05, 200));
+  initializeIsolate(IsolateDate(04, 200));
+  initializeIsolate(IsolateDate(03, 200));
+  initializeIsolate(IsolateDate(02, 200));
+  initializeIsolate(IsolateDate(01, 200));
+}
--- lib/dart_isolate_playground.dart
+++ lib/dart_isolate_playground.dart
@@ -1,1 +1,23 @@
+import 'dart:isolate';
 
+void initializeIsolate(IsolateDate data) =>
+    Isolate.spawn(isolateFunction, data);
+
+void isolateFunction(IsolateDate data) {
+  int _count = 0;
+
+  for (int i = 0; i < data.finalNumber; i++) {
+    _count++;
+
+    if (_count % 100 == 0) {
+      print('isolate: ${data.isolateId}');
+    }
+  }
+}
+
+class IsolateDate {
+  final int isolateId;
+  final int finalNumber;
+
+  IsolateDate(this.isolateId, this.finalNumber);
+}
